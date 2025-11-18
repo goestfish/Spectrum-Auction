f7983e57197160f862d5ceebe6e0d1b123ad89d0
Bowei Kou <kou.76@osu.edu>
2025-11-17 21:51:00 -0500

    test

--- sub/agent_submission.py
+++
@@ -1,5 +0,0 @@
-from my_agent import my_agent_submission
-
-################### SUBMISSION #####################
-agent_submission = my_agent_submission
-####################################################
