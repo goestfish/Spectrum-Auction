b4c06fdb7baab3b81ceeffd1bafec4878363a286
goestfish <114896816+goestfish@users.noreply.github.com>
2025-11-20 01:55:31 -0500

    Update agent_submission.py

--- agent_submission.py
+++ agent_submission.py
@@ -1,5 +1,6 @@
 from my_agent import my_agent_submission
+from test import my_agent_submission2
 
 ################### SUBMISSION #####################
-agent_submission = my_agent_submission
+agent_submission = my_agent_submission2
 ####################################################
